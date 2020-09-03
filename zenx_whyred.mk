c3ca634314fcf8d9adcdbd9b0af1ff91e0261485
adiatul16 <adiatul16@gmail.com>
2020-09-03 19:09:28 +0530

    whyred: Include GAPPS

--- zenx_whyred.mk
+++ zenx_whyred.mk
@@ -21,6 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 
 # Inherit some common ZenX stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
+WITH_GAPPS := true
 TARGET_GAPPS_ARCH := arm64
 $(call inherit-product, vendor/zenx/config/common_full_phone.mk)
 
